b92198eed67ed5229c94c70f9f3ac4b97834cf24
jun maeda <j.maeda@activeltd.co.jp>
2018-04-03 16:26:54 +0900

    最初のコミット
    最初のコミット（この時点でC#側はビルド通らない

---
+++ TestCsharpProject/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TestCsharpProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var v = new TestClassLibrary.Class1())
+            {
+                Console.WriteLine($"Hello World. {v.GetValue()}");
+            }
+            Console.ReadLine();
+        }
+    }
+}
